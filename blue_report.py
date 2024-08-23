7c713046438137f1032d3d53e0577050d43945ad
Mihaela Radian <mihaela.radian@multiversx.com>
2024-08-23 12:26:12 +0300

    Added config and linting files

--- blue_report.py
+++ blue_report.py
@@ -1,7 +1,7 @@
 from fetch_data import DownloadsFetcher
 
 
-#fetcher = DownloadsFetcher.from_package_sites()
-#fetcher.write_report()
-#fetcher.write_json()
-fetcher1 = DownloadsFetcher.from_json_file("./Output/json2024-08-23.txt")
+fetcher = DownloadsFetcher.from_package_sites()
+fetcher.write_report()
+fetcher.write_json()
+# fetcher1 = DownloadsFetcher.from_json_file("./Output/json2024-08-23.txt")
