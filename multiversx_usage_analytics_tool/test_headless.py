5d73e3d9397fbed163f0533cd3cb6d5d9a45efbe
Mihaela Radian <mihaela.radian@multiversx.com>
2024-10-07 14:24:52 +0300

    Added test scripts for pyppeteer

---
+++ multiversx_usage_analytics_tool/test_headless.py
@@ -0,0 +1,16 @@
+import asyncio
+from pyppeteer import launch
+
+
+async def main():
+    try:
+        browser = await launch(headless=True)
+        page = await browser.newPage()
+        await page.goto('https://example.com')
+        await asyncio.sleep(2)  # Keep it open for 5 seconds
+        await browser.close()
+        print('Response OK')
+    except:
+        print('Browser error')
+
+asyncio.get_event_loop().run_until_complete(main())
